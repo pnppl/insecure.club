45d5568d6247e7a06f269903de188ab7423f05d9
pnppl <pnppl@w10.site>
2026-08-15 08:59:13 -0400

    sanitize form input more

--- submit/submit.php
+++ submit/submit.php
@@ -1,23 +1,66 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	$url = htmlspecialchars($_POST['url'] ?? '');
+	$url = str_replace(["\n", "\r"], '', $url);
 	if (empty($url)) {
 		die('Missing URL field');
 	}
+	if (mb_strlen($url, 'UTF-8') > 400) {
+		die('URL too long');
+	}
+
 	$ssl = isset($_POST['ssl']) ? 'yes' : 'no';
+
 	$cat = $_POST['cat'] ?? '';
+	$cat = str_replace(["\n", "\r"], '', $cat);
 	if (empty($cat)) {
 		die('Missing category selection');
 	}
+	if (mb_strlen($cat, 'UTF-8') > 20) {
+		die('Bad category');
+	}
+
 	$desc = htmlspecialchars($_POST['desc'] ?? '');
+	$desc = str_replace(["\n", "\r"], '', $desc);
+	if (mb_strlen($desc, 'UTF-8') > 80) {
+		$desc = mb_substr($desc, 0, 77, 'UTF-8') . "...";
+	}
+
 	$gopher_url = htmlspecialchars($_POST['gopher-url'] ?? '');
+	$gopher_url = str_replace(["\n", "\r"], '', $gopher_url);
+	if (mb_strlen($gopher_url, 'UTF-8') > 400) {
+		die('Gopher URL too long');
+	}
+
 	$gemini_url = htmlspecialchars($_POST['gemini-url'] ?? '');
+	$gemini_url = str_replace(["\n", "\r"], '', $gemini_url);
+	if (mb_strlen($gemini_url, 'UTF-8') > 400) {
+		die('Gemini URL too long');
+	}
+
 	$feed_url = htmlspecialchars($_POST['feed-url'] ?? '');
+	$feed_url = str_replace(["\n", "\r"], '', $feed_url);
+	if (mb_strlen($feed_url, 'UTF-8') > 400) {
+		die('Feed URL too long');
+	}
+
 	$css_opt = isset($_POST['css-opt']) ? 'yes' : 'no';
+
 	$libre = isset($_POST['libre']) ? 'yes' : 'no';
+
 	$button = htmlspecialchars($_POST['button'] ?? '');
+	$button = str_replace(["\n", "\r"], '', $button);
+	if (mb_strlen($button, 'UTF-8') > 400) {
+		die('Button URL too long');
+	}
+
 	$email = filter_var($_POST['email'] ?? '', FILTER_VALIDATE_EMAIL);
+
 	$message = htmlspecialchars($_POST['message'] ?? '');
+	$message = str_replace(["\n", "\r"], '(NEWLINE)', $message);
+	if (mb_strlen($message, 'UTF-8') > 5000) {
+		die('Message too long');
+	}
 
 	$to = 'submission@insecure.club';
 	$subject = "New submission: $url";
@@ -27,9 +70,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	$body .= "ssl: $ssl\n";
 	$body .= "cat: $cat\n";
 	if (!empty($desc)) {
-		if (mb_strlen($desc, 'UTF-8') > 80) {
-			$desc = mb_substr($desc, 0, 77, 'UTF-8') . "...";
-		}
 		$body .= "desc: $desc\n";
 	}
 	if (!empty($gopher_url)) {
